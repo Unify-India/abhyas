5fd833e435829e8b99586c6d192d2f10470688e7
IAM5K <sandeep2k2656@gmail.com>
2024-12-26 21:31:08 +0530

    Setup route and pages for school pages

--- src/app/pages/school/school-routing.module.ts
+++ src/app/pages/school/school-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/auth/guards/auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'profile',
+        loadComponent: () => import('./profile/profile.page').then((m) => m.ProfilePage),
+      },
+      {
+        path: 'requests/join-requests',
+        loadComponent: () => import('./requests/join-requests/join-requests.page').then((m) => m.JoinRequestsPage),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
