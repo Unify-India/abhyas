f17e0ed629643fb584d3906c711e0bd0fca1726f
IAM5K <sandeep2k2656@gmail.com>
2024-12-26 21:28:49 +0530

    Setup route and pages for exam pages

--- src/app/pages/exam/exam-routing.module.ts
+++ src/app/pages/exam/exam-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/auth/guards/auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    canLoad: [AuthGuard],
+    children: [
+      {
+        path: 'exam-list',
+        loadComponent: () => import('./components/exam-list/exam-list.component').then((m) => m.ExamListComponent),
+      },
+      {
+        path: 'exam-detail/:id',
+        canLoad: [AuthGuard],
+        loadComponent: () =>
+          import('./components/exam-details/exam-details.component').then((m) => m.ExamDetailsComponent),
+      },
+      {
+        path: 'create-exam',
+        canLoad: [AuthGuard],
+        loadComponent: () => import('./create-exam/create-exam.page').then((m) => m.CreateExamPage),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
