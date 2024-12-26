5f5a6e36329822cf678f161cb6cf95f4a2b9c83f
IAM5K <sandeep2k2656@gmail.com>
2024-12-26 21:29:09 +0530

    Created exam submission component

---
+++ src/app/pages/exam/modals/exam-submission/exam-submission.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exam-submission',
+  templateUrl: './exam-submission.component.html',
+  styleUrls: ['./exam-submission.component.scss'],
+})
+export class ExamSubmissionComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
