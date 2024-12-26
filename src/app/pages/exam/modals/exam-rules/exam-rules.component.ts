437dbc44a8c723aed8bfbc3174adb626a9ebe610
IAM5K <sandeep2k2656@gmail.com>
2024-12-26 21:29:24 +0530

    Created exam rules component

---
+++ src/app/pages/exam/modals/exam-rules/exam-rules.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exam-rules',
+  templateUrl: './exam-rules.component.html',
+  styleUrls: ['./exam-rules.component.scss'],
+})
+export class ExamRulesComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
